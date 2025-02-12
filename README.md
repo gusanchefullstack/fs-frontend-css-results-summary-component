668afd112de8231ec8e78ee277bed2e9e0242048
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-02-12 10:05:59 -0500

    Add html structure and basic tailwind classes

--- README.md
+++ README.md
@@ -42,8 +42,8 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [@Frontendmentor.io](https://your-solution-url.com)
+- Live Site URL: [@Vercel.com](https://fs-frontend-css-results-summary-component.vercel.app/)
 
 ## My process
 
--- src/components/ResultsSummary.jsx
+++ src/components/ResultsSummary.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const ResultsSummary = () => {
   return (
-    <div>
-        Your Result
-  76
-  of 100
-
-  Great
-  You scored higher than 65% of the people who have taken these tests.
-  
-  Summary
-
-  Reaction
-  80 / 100
-
-  Memory
-  92 / 100
-
-  Verbal
-  61 / 100
-
-  Visual
-  72 / 100
-
-  Continue
-
+    <div className="flex flex-row text-lg font-summary-card">
+      <div className="">
+        <h1>Your Result</h1>
+        <div>76 of 100</div>
+        <p>Great</p>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+      <div className="">
+        <h2>Summary</h2>
+        <div className="summary-card">
+          <div className="summary-item">Reaction</div>
+          <div className="item-score">80 / 100</div>
+        </div>
+        <div className="summary-card">
+          <div className="summary-item">Memory</div>
+          <div className="item-score">92 / 100</div>
+        </div>
+        <div className="summary-card">
+          <div className="summary-item">Verbal</div>
+          <div className="item-score">61 / 100</div>
+        </div>
+        <div className="summary-card">
+          <div className="summary-item">Visual</div>
+          <div className="item-score">72 / 100</div>
+        </div>
+        <button>Continue</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResultsSummary
+export default ResultsSummary;
