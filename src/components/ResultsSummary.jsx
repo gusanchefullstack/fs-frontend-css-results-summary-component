0f2744c5dfd8589654a329f8e6aefa6b211ee549
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-02-12 22:28:23 -0500

    Add styles for mobile

--- src/components/ResultsSummary.jsx
+++ src/components/ResultsSummary.jsx
@@ -1,21 +1,21 @@
 const ResultsSummary = () => {
   return (
-    <div className="flex flex-row text-lg font-summary-card bg-white-card w-[736px] h-[512px] rounded-[32px]">
-      <div className="w-[368px] h-h-full bg-linear-to-b from-light-slate-blue-background to-light-royal-blue-background text-center pt-[38px] rounded-[32px]">
-        <h1 className="font-bold text-2xl text-light-lavender">Your Result</h1>
-        <div className="mx-auto flex flex-col justify-center items-center h-[200px] w-[200px] bg-linear-to-b from-violet-blue-circle to-persian-blue-circle rounded-full mt-[35px]">
-          <div className="font-extrabold text-white-card text-7xl">76</div>
-          <div className="text-lg font-semibold text-light-lavender">
+    <div className="flex flex-row max-sm:flex-wrap max-sm:justify-center text-lg font-summary-card bg-white-card w-[736px] h-[512px] rounded-[32px] ">
+      <div className="w-[368px] max-sm:w-[375px] h-full max-sm:h-[356px] bg-linear-to-b from-light-slate-blue-background to-light-royal-blue-background text-center pt-[38px] max-sm:pt-6 rounded-[32px] max-sm:rounded-t-none">
+        <h1 className="font-bold text-2xl max-sm:text-lg text-light-lavender">Your Result</h1>
+        <div className="mx-auto flex flex-col justify-center items-center h-[200px] w-[200px] max-sm:h-[140px] max-sm:w-[140px] bg-linear-to-b from-violet-blue-circle to-persian-blue-circle rounded-full mt-[35px] max-sm:mt-6">
+          <div className="font-extrabold text-white-card text-7xl max-sm:text-[56px]">76</div>
+          <div className="text-lg max-sm:text-base font-semibold text-light-lavender">
             of 100
           </div>
         </div>
-        <p className="text-white-card font-bold text-[32px] mt-7">Great</p>
-        <p className="font-medium text-lg text-light-lavender px-[54px]">
+        <p className="text-white-card font-bold text-[32px] max-sm:text-2xl mt-7 max-sm:mt-5">Great</p>
+        <p className="font-medium text-lg max-sm:text-base text-light-lavender px-[54px]">
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
-      <div className="w-[368px] h-full p-[38px]">
-        <h2 className="text-dark-gray-blue font-bold text-2xl">Summary</h2>
+      <div className="w-[368px] max-sm:w-[375px] h-full max-sm:h-[356px] p-[38px]">
+        <h2 className="text-dark-gray-blue font-bold text-2xl max-sm:text-lg">Summary</h2>
         <div className="flex flex-col gap-4 mt-7">
           <div className="summary-card bg-light-red/5">
             <div className="summary-title-container">
@@ -48,7 +48,7 @@ const ResultsSummary = () => {
             <div className="item-score"> <span>73</span> <span>/ 100</span></div>
           </div>
         </div>
-        <button className="h-14 w-full rounded-[128px] bg-dark-gray-blue text-white-card font-bold text-lg mt-[41px] hover:cursor-pointer hover:bg-gradient-to-b hover:from-light-slate-blue-background hover:to-light-royal-blue-background">
+        <button className="h-14 w-full rounded-[128px] bg-dark-gray-blue text-white-card font-bold text-lg max-sm:text-lg mt-[41px] max-sm:mt-6 hover:cursor-pointer hover:bg-gradient-to-b hover:from-light-slate-blue-background hover:to-light-royal-blue-background">
           Continue
         </button>
       </div>
